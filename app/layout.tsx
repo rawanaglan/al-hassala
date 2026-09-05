e89fbf11470124c82b5e1fec5bb84ccf331d285b
Rawana Aglan <rawanaglan2005@gmail.com>
2026-09-05 17:25:13 +0300

    Trigger deployment with correct remote URL

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+
 export const metadata: Metadata = {
   title: "الحصالة | Al-Hassala",
   description: "أفكار صغيرة.. تصنع حياة كبيرة",
